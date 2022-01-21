73cf9a090b404e0f8ec7c7d7e81aab0584205d18
Dhashin Moodley <dhashin@stitch.money>
2022-01-22 00:19:16 +0200

    changes

--- index.js
+++ index.js
@@ -77,12 +77,12 @@ const readUsers = async()=>{
 
 const execute = async ()=> {
     // Your async task will execute with await
+while (1===1) {
+    await writeUsers().then(r => {
+        console.log('Write to db success');
 
-        await writeUsers().then(r => {
-            console.log('Write to db success');
-
-        })
-
+    })
+}
 }
 
 

9006f32e1f7926286d659ea95572adfa31ca6aef
Dhashin Moodley <dhashin@stitch.money>
2022-01-24 23:58:20 +0200

    changes

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ async function writeUsers() {
         minute: "numeric"}).replaceAll('/','-');
     console.log(currentDate);
     const docRef = await db.collection('environment').doc(`${currentDate}`);
+    const docRefCooler = await db.collection('cooler').doc('1');
+    const docRefHeater = await db.collection('heater').doc('1');
+    const docRefHumidifier = await db.collection('humidifier').doc('1');
+    const docRefExtractor = await db.collection('extractor').doc('1');
 
     let finaltemp
     let finalhumidity
@@ -66,18 +70,62 @@ async function writeUsers() {
     try {
         const res = await sensor.read(22, 4);
 
-        finaltemp = res.temperature.toFixed(1) + '°C';
-        finalhumidity = res.humidity.toFixed(1)  +  '%';
+        finaltemp = res.temperature.toFixed(1) ;
+        finalhumidity = res.humidity.toFixed(1);
 
         console.log(
-            `temp: ${finaltemp}` +
-            `humidity: ${finalhumidity}`
+            `temp: ${finaltemp}°C` +
+            `humidity: ${finalhumidity}%`
         );
            const result= await docRef.set({
-                temp : `${finaltemp}`,
-                humidity : `${finalhumidity}`
+                temp : `${finaltemp}°C`,
+                humidity : `${finalhumidity}%`
             })
         console.log(result);
+        if(finaltemp > 24){
+            console.log('Turning on cooler')
+            await docRefCooler.set({
+                status : 'on'
+            })
+            console.log('Turning off heater')
+            await docRefHeater.set({
+                status : 'off'
+            })
+        }else if(finaltemp < 21 ){
+            console.log('Turning on heater')
+            await docRefHeater.set({
+                status : 'on'
+            })
+            console.log('Turning off cooler')
+            await docRefCooler.set({
+                status : 'off'
+            })
+        }
+
+        if(finalhumidity < 60) {
+            console.log('Turning on humidifier')
+            await docRefHumidifier.set({
+                status : 'on'
+            })
+        }else if(finalhumidity > 80){
+            console.log('Turning off humidifier')
+            await docRefHumidifier.set({
+                status : 'off'
+            })
+        }
+
+        if(finalhumidity < 70) {
+            console.log('Turning off extractor')
+            await docRefExtractor.set({
+                status : 'off'
+            })
+        }else if(finalhumidity > 90){
+            console.log('Turning on extractor')
+            await docRefExtractor.set({
+                status : 'on'
+            })
+        }
+
     } catch (err) {
         console.error("Failed to read sensor data:", err);
     }

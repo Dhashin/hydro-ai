63b689090b91970c84c5a73214b4a285bd10c465
Dhashin Moodley <dhashin@stitch.money>
2022-01-30 15:50:30 +0200

    more changes

--- test2.js
+++ test2.js
@@ -1,7 +1,17 @@
 const gpio = require('rpi-gpio');
-gpio.setup(40, gpio.DIR_OUT);
 
-    gpio.write(40, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin');
-    });
+
+
+    async function setup(){
+        await gpio.setup(40, gpio.DIR_OUT);
+    }
+
+    setup().then(()=>{
+            gpio.write(40, true, function(err) {
+                if (err) throw err;
+                console.log('Written to pin');
+            });
+
+        }
+
+    )

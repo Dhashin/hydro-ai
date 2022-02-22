ed5cf55b62c324a88bf3561ad3eae099762ce58b
Dhashin Moodley <dhashin@stitch.money>
2022-02-22 11:44:32 +0200

    changes

--- water-level.js
+++ water-level.js
@@ -1,5 +1,5 @@
 const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 
-const gpio5 = new Gpio(21, 'in');
+const gpio5 = new Gpio(5, 'in');
 console.log('Current state 21 is ', gpio5.readSync())
 gpio5.unexport()

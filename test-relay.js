64270e5a665795c76e7ee9390932935368616095
Dhashin Moodley <dhashin@stitch.money>
2022-02-26 14:05:53 +0200

    changes

--- test-relay.js
+++ test-relay.js
@@ -3,11 +3,19 @@ const {Gpio} = require("onoff");
 
 const gpio26 = new Gpio(26, 'out');
 gpio26.writeSync(1);
+console.log('Current state 26 is ', gpio26.readSync())
 gpio26.writeSync(0);
+console.log('Current state 26 is ', gpio26.readSync())
 gpio26.writeSync(1);
+console.log('Current state 26 is ', gpio26.readSync())
 gpio26.writeSync(0);
+console.log('Current state 26 is ', gpio26.readSync())
 gpio26.writeSync(1);
+console.log('Current state 26 is ', gpio26.readSync())
 gpio26.writeSync(0);
+console.log('Current state 26 is ', gpio26.readSync())
 gpio26.writeSync(1);
+console.log('Current state 26 is ', gpio26.readSync())
 gpio26.writeSync(0);
+console.log('Current state 26 is ', gpio26.readSync())
 gpio26.unexport();

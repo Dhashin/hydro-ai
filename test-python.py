dc93c5aaf8026e97911e5d56bb17aabac33d5c5f
Dhashin Moodley <dhashin@stitch.money>
2022-02-27 08:37:48 +0200

    changes

---
+++ test-python.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(26,GPIO.OUT)
+print "LED on"
+GPIO.output(26,GPIO.HIGH)
+time.sleep(1)
+print "LED off"
+GPIO.output(26,GPIO.LOW)
+time.sleep(1)
+print "LED on"
+GPIO.output(26,GPIO.HIGH)
+time.sleep(1)
